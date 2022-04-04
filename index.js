d3915190682fe2d6eddea8b3fdb7aa8a41f55148
PrashantRaj18198 <prashant@argonaut.dev>
2022-04-04 13:00:36 +0530

    remove blank line

--- index.js
+++ index.js
@@ -3,8 +3,7 @@ const port = process.env.PORT || 3000;
 
 const server = http.createServer((req, res) => {
   res.statusCode = 200;
-  const msg = 'Hello Node! V2\n'
-  
+  const msg = 'Hello Node! V2\n'  
   res.end(msg);
 });
 
